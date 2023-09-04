c3834294fde65e7e328772a799c8e6e394b59ddb
Luiz Felipe Burgatt Jolo <luiz.jolo@grupopereira.local>
2023-09-04 09:56:13 -0400

    Correção e melhoria de calculo de porcentagem. Estava apresentando valores errados na coluna porcentagem em casos de valores muito próximos, exemplo para simular o teste: btc 130700.22 e 130700.18

--- utils/porcentagem.py
+++ utils/porcentagem.py
@@ -2,24 +2,14 @@
 def calc_porc(anterior, nova):
     anterior = anterior
     nova = nova
+    x = round((nova-anterior)/anterior * 100,2)
 
     if anterior < nova:
-        x = (nova*100.00) / anterior
-        x = 100.00 - x
-        x = str(x)
-        x = x.replace('-', '')
-        x = float(x)
-        x = round(x, 2)
         x = ("+" + str(x))
         return x
 
     elif anterior > nova:
-        x = (nova*100.00) / anterior
-        x = 100.00 - x
         x = str(x)
-        x = float(x)
-        x = round(x, 2)
-        x = ("-" + str(x))
         return x
 
     else:
